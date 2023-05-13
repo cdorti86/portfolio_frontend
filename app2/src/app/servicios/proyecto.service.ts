a53c00315babe1e91e9fd41b1d52189f90067c4d
cdcorti86 <cdcorti86@gmail.com>
2023-05-12 23:48:08 -0300

    actualizar service de proyecto

--- app2/src/app/servicios/proyecto.service.ts
+++ app2/src/app/servicios/proyecto.service.ts
@@ -13,22 +13,22 @@ export class ProyectoService {
   constructor(private http:HttpClient) { }
  
   public obtenerdatosproyecto(): Observable<any> {
-    return this.http.get<Proyecto[]>(`${this.apiBaseUrl}/lista/Proyecto`); 
+    return this.http.get<Proyecto[]>(`${this.apiBaseUrl}/lista/proyectos`); 
 }
 
 public obtenerunaproyecto(Id: number): Observable<Proyecto> {
-  return this.http.get<Proyecto>(`${this.apiBaseUrl}/buscar/Proyecto/${Id}`); 
+  return this.http.get<Proyecto>(`${this.apiBaseUrl}/buscar/proyecto/${Id}`); 
 }
 public altaproyecto(proyecto: Proyecto): Observable<Proyecto> {
   console.log(proyecto)  
-  return this.http.post<Proyecto>(`${this.apiBaseUrl}/nuevo/Proyecto`, proyecto);
+  return this.http.post<Proyecto>(`${this.apiBaseUrl}/nuevo/proyecto`, proyecto);
   } 
 
   public actualizarproyecto(proyecto: Proyecto):Observable <any> {
-    return this.http.put(`${this.apiBaseUrl}/modificar/Proyecto`, proyecto);
+    return this.http.put(`${this.apiBaseUrl}/modificar/proyecto`, proyecto);
   }
 
   public eliminarproyecto(Id: number):Observable <void>{
-    return this.http.delete<void>(`${this.apiBaseUrl}/delete/Proyecto/${Id}`);
+    return this.http.delete<void>(`${this.apiBaseUrl}/delete/proyecto/${Id}`);
   }
 }
